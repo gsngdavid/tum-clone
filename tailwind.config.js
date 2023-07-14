6af0258e6a06d81b3af0f00b6668e6da971d6553
gsngdavid <gsngdavid@gmail.com>
2023-07-14 17:22:19 +0200

    feat navigation, news and hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./build/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "blue-primary": "#3070b3",
+        "blue-hover": "#14519A",
+        "blue-secondary": "#0a2d57",
+        "blue-dark": "#072140",
+        "gray-light": "#E8ECEF",
+      },
+      fontFamily: {
+        "roboto": "'Roboto', sans-serif;"
+      }
+    },
   },
   plugins: [],
 }
